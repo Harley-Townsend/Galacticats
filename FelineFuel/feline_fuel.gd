3665fa464dc59ea413db22c5bbcd7ec5a3707228
Harley Townsend <harleytownsend.wrk@gmail.com>
2026-02-11 16:45:28 +0000

    started gamestate

--- FelineFuel/feline_fuel.gd
+++ FelineFuel/feline_fuel.gd
@@ -1,11 +1,23 @@
 extends Node3D
 
+var can_enter = false
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("Player"):
-		body.near_door()
+		var prompt = body.get_node("PlayerUserInterface/CanvasLayer/Label")
+		if prompt:
+			prompt.text = "Enter [ E ]"
+			prompt.visible = true
+		can_enter = true
 		
-
 func _on_area_3d_body_exited(body):
 	if body.is_in_group("Player"):
-		body.not_near_door()
+		var prompt = body.get_node("PlayerUserInterface/CanvasLayer/Label")
+		if prompt:
+			prompt.visible = false
+		can_enter = false
+		
+func _process(delta):
+	if can_enter == true:
+		if Input.is_action_just_pressed("interact"):
+			get_tree().change_scene_to_file("res://FelineFuel/FelineFuelInterior/feline_fuel_interior.tscn")
--- Player/Player.gd
+++ Player/Player.gd
@@ -5,7 +5,6 @@ var gravity = 20
 var vel = Vector3.DOWN
 var jump_strength = 10
 var can_board = false
-var can_enter = false
 var state = "normal"
 
 @onready var pivot = $Pivot
@@ -15,9 +14,6 @@ var state = "normal"
 func _input(event):
 	if can_board == true and Input.is_action_just_pressed("interact"):
 		board()
-	if can_enter == true and Input.is_action_just_pressed("interact"):
-		enter()
-		
 
 
 func _physics_process(delta):
@@ -64,24 +60,11 @@ func _physics_process(delta):
 		var look_direction = Vector2(vel.z, vel.x)
 		model.rotation.y = lerp_angle(model.rotation.y, look_direction.angle(), delta*5)
 	
-	
-		
 
 signal boarding_complete
 
 func _process(delta):
 	pivot.position = position
-	
-func near_door():
-	can_enter = true
-	$PlayerUserInterface/CanvasLayer/EnterLabel.visible = true
-func not_near_door():
-	can_enter = false
-	$PlayerUserInterface/CanvasLayer/EnterLabel.visible = false
-	
-func enter():
-	can_enter = false
-	get_tree().change_scene_to_file("res://FelineFuel/FelineFuelInterior/feline_fuel_interior.tscn")
 	
 func board():
 	can_board = false
@@ -93,7 +76,8 @@ func board():
 
 func near_ship():
 	can_board = true
-	$PlayerUserInterface/CanvasLayer/BoardLabel.visible = true
+	$PlayerUserInterface/CanvasLayer/Label.text = "Board [ E ]"
+	$PlayerUserInterface/CanvasLayer/Label.visible = true
 func not_near_ship():
 	can_board = false
-	$PlayerUserInterface/CanvasLayer/BoardLabel.visible = false
+	$PlayerUserInterface/CanvasLayer/Label.visible = false
